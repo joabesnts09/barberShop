75f1b9d8f26aace44089ea79826db0ee1b3564fe
joabesnts09 <joabesilva1233@gmail.com>
2024-02-01 23:15:43 -0300

    feat: add footer component

---
+++ src/app/(home)/_components/BarberShopItem.tsx
@@ -0,0 +1,56 @@
+import { Badge } from '@/app/_components/ui/badge'
+import { Button } from '@/app/_components/ui/button'
+import { Card, CardContent } from '@/app/_components/ui/card'
+import { Barbershop } from '@prisma/client'
+import { StarIcon } from 'lucide-react'
+import Image from 'next/image'
+
+interface IBarberShopItemProps {
+    barbershop: Barbershop
+}
+
+const BarberShopItem = ({ barbershop }: IBarberShopItemProps) => {
+    return (
+        <>
+            <Card className="min-w-[167px] max-w-[167px] rounded-2xl">
+                <CardContent className="px-0 py-0 ">
+                    <div className="flex flex-col gap-2 py-5 pl-5 flex-[3] h-[159px] w-full relative">
+
+                        <div className='absolute top-2 left-2 z-50'>
+                            <Badge variant='secondary' className='flex gap-1 items-center opacity-90'>
+                                <StarIcon className='fill-primary text-primary' size={12}/>
+                                <span className='text-xs'>5,0</span>
+                            </Badge>
+                        </div>
+
+                        <Image
+                            src={barbershop.imageUrl}
+                            alt={barbershop.name}
+                            fill
+                            style={{
+                                objectFit: 'cover',
+                            }}
+                            className="rounded-2xl"
+                        />
+                    </div>
+
+                    <div className='px-2 pb-3'>
+                        <h2 className="font-bold mt-2 overflow-hidden text-ellipsis text-nowrap">
+                            {barbershop.name}
+                        </h2>
+
+                        <p className="text-sm text-gray-400 overflow-hidden text-ellipsis text-nowrap">
+                            {barbershop.address}
+                        </p>
+
+                        <Button variant="secondary" className="w-full mt-3">
+                            Resevar
+                        </Button>
+                    </div>
+                </CardContent>
+            </Card>
+        </>
+    )
+}
+
+export default BarberShopItem
--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -1,12 +1,18 @@
 import Image from 'next/image'
 import { main } from 'ts-node/dist/bin'
-import Header from '../_components/header'
 import { format } from 'date-fns'
 import { ptBR } from 'date-fns/locale'
-import Search from './_components/search'
-import BookingItem from '../_components/bookingItem'
+import Header from '../_components/Header'
+import Search from './_components/Search'
+import BookingItem from '../_components/BookingItem'
+import { db } from '../_lib/prisma'
+import BarberShopItem from './_components/BarberShopItem'
 
-export default function Home() {
+
+export default async function Home() {
+    
+    const barberShop = await db.barbershop.findMany({})
+
     return (
         <div>
             <Header />
@@ -29,6 +35,29 @@ export default function Home() {
                 <h2 className='text-xs uppercase text-gray-400 font-bold mb-3'>Agendamentos</h2>
                 <BookingItem/>
                 
+            </div>
+
+            <div className='mt-6'>
+                <h2 className='px-5 text-xs uppercase text-gray-400 font-bold mb-3'>Recomendados</h2>
+
+
+                <div className='flex px-5 gap-2 overflow-x-auto [&::-webkit-scrollbar]:hidden'>
+                    {barberShop.map((barbershop) => (
+                       <BarberShopItem key={barbershop.id}  barbershop={barbershop}/>
+                    ))}
+                </div>
+            </div>
+
+
+            <div className='mt-6 mb-[4.5rem]'>
+                <h2 className='px-5 text-xs uppercase text-gray-400 font-bold mb-3'>Populares</h2>
+
+
+                <div className='flex px-5 gap-2 overflow-x-auto [&::-webkit-scrollbar]:hidden'>
+                    {barberShop.map((barbershop) => (
+                       <BarberShopItem key={barbershop.id}  barbershop={barbershop}/>
+                    ))}
+                </div>
             </div>
         </div>
     )
